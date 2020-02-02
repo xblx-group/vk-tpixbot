0d30273bfc9a9f63c811fdc4974ba00109ce2135
TTaaaaRRR <fedor.r-n@yandex.ru>
2020-02-02 20:36:06 +0300

    using qsearch

--- bot/tpix-api.js
+++ bot/tpix-api.js
@@ -29,7 +29,7 @@ var api = {
         }
     },
     options: (request) => {
-        var adr = "https://api.openpix.ru/api/v0.3/";
+        var adr = "https://api.openpix.ru/api/v0.4/";
         adr += request.method + "?";
         if(request.params){
             Object.keys(request.params).forEach((i, e, a) => {
