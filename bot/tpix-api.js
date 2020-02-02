87387312c4473ae10bdb43940a92096f94d9b765
TTaaaaRRR <fedor.r-n@yandex.ru>
2020-02-02 16:41:31 +0300

    2.1

---
+++ bot/tpix-api.js
@@ -0,0 +1,43 @@
+const http = require('request-promise');
+var api = {
+    train: {
+        search: (request) => {
+            var query = request.query;
+            var count = request.count || 5;
+            var st = request.st || 0;
+            var state = request.state || 0;
+            return(http(api.options({method: "train/search", params: {"query": query, "count": count, "st": st, "state": state}})));
+        },
+        qsearch: (request) => {
+            var query = request.query;
+            var count = 10 || request.count;
+            return(http(api.options({method: "train/qsearch", params: {"query": query, "count": count}})));
+        },
+        get: (request) => {
+            var id = request.id;
+            return(http(api.options({method: "train/get", params: {"id": id}})));
+        }
+    },
+    photo: {
+        get: (request) => {
+            var id = request.id;
+            var quick = 0 || request.quick;
+            return(http(api.options({method: "photo/get", params: {"id": id, "quick": quick}})));
+        },
+        random: () => {
+            return(http(api.options({method: "photo/random"})));
+        }
+    },
+    options: (request) => {
+        var adr = "https://api.openpix.ru/api/v0.3/";
+        adr += request.method + "?";
+        if(request.params){
+            Object.keys(request.params).forEach((i, e, a) => {
+                adr += i + "=" + encodeURIComponent(request.params[i]) + "&";
+            });
+        }
+       
+        return({method: 'GET', uri: adr, json: true});
+    }
+};
+module.exports = api;
